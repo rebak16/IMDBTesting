a8dd7eab969aaddcc3df694def335abf2bc000a1
rebak16 <rebakbalazs@gmail.com>
2020-07-09 18:47:42 +0200

    listTest added

--- src/main/java/com/qa/runner/TestRunner.java
+++ src/main/java/com/qa/runner/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"/home/rebak/Codecool/Codecool Rooms/Test Automation/Jobhunt/IMDBTesting/src/main/java/com/qa/features/checkList.feature"},
+@CucumberOptions(features = {"/home/rebak/Codecool/Codecool Rooms/Test Automation/Jobhunt/IMDBTesting/src/main/java/com/qa/features/advancedSearch.feature"},
         glue = {"com.qa.stepDefinitions"},
 //, format= {"pretty","html:test-outout"}
         monochrome = true,
